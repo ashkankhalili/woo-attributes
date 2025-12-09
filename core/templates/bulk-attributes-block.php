7832a23b3ea40af3ceb976e6689bea0a91e405f6
Ashkan Khalili <s.ashkankhalili@gmail.com>
2025-12-09 20:13:20 +0330

    v1.0.7
    Manual Attributes bug fix

--- core/templates/bulk-attributes-block.php
+++ core/templates/bulk-attributes-block.php
@@ -1,90 +1,124 @@
 <div id="bulk_attributes_block" class="panel woocommerce_options_panel">
-    <script>
-    ( function( $ ) {
-        $("document").ready(function(){
-            $("select.dropdown").dropdown();
-            $('#attribute-submit')
-            .click(function(e) {
-                e.preventDefault();
-                var data = {
-                    'post_id': <?php echo $product->get_id()?>,
-                    'product_type': '<?php echo $product->get_type()?>',
-                    'action': 'woocommerce_save_attributes',
-                    'security': '<?php echo wp_create_nonce('save-attributes');?>'
-                };
-                data['data'] = new Object();
-                data['data']['attribute_names'] = new Object();
-                data['data']['attribute_values'] = new Object();
-                data['data']['attribute_visibility'] = new Object();
-                var values = null;
-                var co = 0;
-                <?php foreach ($attributes as $a) :?>
-                    select = $('#attr-dropdown-<?php echo $a->attribute_id?>');
-                    var values = select.parent().dropdown('get value');
-                    if(values.length != 0){
-                        data['data']['attribute_names'][co] = "<?php echo "pa_".$a->attribute_name ?>";
-                        data['data']['attribute_values'][co] = values;
-                        data['data']['attribute_visibility'][co] = 1
-                        co += 1;
-                    }
-                <?php endforeach ?>
-                var url = "<?php echo admin_url('admin-ajax.php') ?>";
-                data['data'] = $.param(data['data']);
-                $.ajax({
-                    url: url,
-                    type: 'post',
-                    data: $.param(data),
-                    beforeSend: function(){
-                        $('#attribute-submit').addClass('loading');
-                    },
-                    complete:function(data){
-    $('#attribute-submit').removeClass('loading');
-    location.reload();
-}
+<script>
+(function($){
 
-                });
-            });
-            <?php foreach ($attributes as $a) :?>
-            $("#attr-dropdown-<?php echo $a->attribute_id; ?>").dropdown('set selected', [
-                <?php
-                $selected_items = get_the_terms($product->get_id(), "pa_".$a->attribute_name);
-                if (!empty($selected_items)) {
-                    if (!is_wp_error($selected_items)) {
-                        $flag = false;
-                        foreach ($selected_items as $term) {
-                            if ($flag) {
-                                echo ", ";
-                            }
-                            echo "'".$term->name."'";
-                            $flag = true;
-                        }
-                    }
-                }
-                ?>
-            ]);
-            <?php endforeach ?>
+$("document").ready(function(){
+
+    $("select.dropdown").dropdown();
+
+    $('#attribute-submit').click(function(e){
+        e.preventDefault();
+
+        var data = {
+            'post_id': <?php echo $product->get_id()?>,
+            'product_type': '<?php echo $product->get_type()?>',
+            'action': 'woocommerce_save_attributes',
+            'security': '<?php echo wp_create_nonce('save-attributes');?>'
+        };
+
+        data['data'] = {};
+        data['data']['attribute_names'] = {};
+        data['data']['attribute_values'] = {};
+        data['data']['attribute_visibility'] = {};
+        data['data']['attribute_variation'] = {};
+        data['data']['attribute_position'] = {};
+
+        var co = 0;
+
+        /* ALWAYS collect current custom attributes from WooCommerce attribute form fields */
+        $('[name^="attribute_names["]').each(function(index){
+            var name = $(this).val();
+            if(name && name.indexOf('pa_') === -1){
+                var val = $('[name="attribute_values['+index+']"]').val() || '';
+                var vis = $('[name="attribute_visibility['+index+']"]').prop('checked') ? 1 : 0;
+                var varr= $('[name="attribute_variation['+index+']"]').prop('checked') ? 1 : 0;
+
+                data['data']['attribute_names'][co]      = name;
+                data['data']['attribute_values'][co]    = val;
+                data['data']['attribute_visibility'][co]= vis;
+                data['data']['attribute_variation'][co] = varr;
+                data['data']['attribute_position'][co]  = co;
+                co++;
+            }
         });
-    }( jQuery3_1_1 ) );
-    </script>
 
-    <div class="ui form" style="margin: 1em;">
-        <?php foreach ($attributes as $a) :?>
-            <div class="field">
-                <label><?php echo $a->attribute_label ?></label>
-                <select class="ui search dropdown attribute" multiple="" id="<?php echo 'attr-dropdown-'.$a->attribute_id ?>">
-                    <?php
-                        $choices = get_terms([
-                            'taxonomy' => "pa_".$a->attribute_name,
-                            'hide_empty' => false
-                        ]);
-                    ?>
-                    <?php foreach ($choices as $choice) :?>
-                        <option value="<?php echo $choice->term_id ?>" > <?php echo $choice->name ?> </option>
-                    <?php endforeach ?>
-                </select>
-            </div>
+        /* taxonomy attributes from your plugin UI */
+        <?php foreach ($attributes as $a) : ?>
+        var select = $('#attr-dropdown-<?php echo $a->attribute_id?>');
+        var values = select.parent().dropdown('get value');
+
+        if(values && values.length){
+            var list = values.map(function(v){
+                return $("#attr-dropdown-<?php echo $a->attribute_id?> option[value='"+v+"']").text();
+            });
+
+            data['data']['attribute_names'][co]      = "pa_<?php echo $a->attribute_name ?>";
+            data['data']['attribute_values'][co]    = list.join(',');
+            data['data']['attribute_visibility'][co]= 1;
+            data['data']['attribute_variation'][co] = 0;
+            data['data']['attribute_position'][co]  = co;
+            co++;
+        }
         <?php endforeach ?>
-        <button id="attribute-submit" class="ui button" type="button">ذخیره ویژگی‌ها</button>
+
+        var url = "<?php echo admin_url('admin-ajax.php') ?>";
+
+        data['data'] = $.param(data['data']);
+
+        $.ajax({
+            url: url,
+            type: 'post',
+            data: $.param(data),
+            beforeSend:function(){
+                $('#attribute-submit').addClass('loading');
+            },
+            complete:function(){
+                $('#attribute-submit').removeClass('loading');
+                location.reload();
+            }
+        });
+    });
+
+    <?php foreach ($attributes as $a) : ?>
+    $("#attr-dropdown-<?php echo $a->attribute_id; ?>").dropdown('set selected', [
+        <?php
+        $selected_items = get_the_terms($product->get_id(), "pa_".$a->attribute_name);
+        if (!empty($selected_items) && !is_wp_error($selected_items)) {
+            $flag=false;
+            foreach ($selected_items as $term){
+                if ($flag) echo ", ";
+                echo "'".$term->term_id."'";
+                $flag=true;
+            }
+        }
+        ?>
+    ]);
+    <?php endforeach ?>
+
+});
+
+})(jQuery3_1_1);
+</script>
+
+
+<div class="ui form" style="margin:1em;">
+<?php foreach ($attributes as $a) : ?>
+    <div class="field">
+        <label><?php echo $a->attribute_label ?></label>
+        <select class="ui search dropdown attribute" multiple="" id="<?php echo 'attr-dropdown-'.$a->attribute_id ?>">
+        <?php
+            $choices = get_terms([
+                'taxonomy' => "pa_".$a->attribute_name,
+                'hide_empty' => false
+            ]);
+        ?>
+        <?php foreach ($choices as $choice) : ?>
+            <option value="<?php echo $choice->term_id ?>"><?php echo $choice->name ?></option>
+        <?php endforeach ?>
+        </select>
     </div>
+<?php endforeach ?>
+<button id="attribute-submit" class="ui button" type="button">ذخیره ویژگی‌ها</button>
 </div>
-TESTE AND OK
+
+</div>
